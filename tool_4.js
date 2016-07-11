01c1dce3aa4fe6c0c997a91586a70455e9545226
Kenneth Chan <kcm_kennethchan@kcbh.com.hk>
2016-07-11 13:56:59 +0800

    first-commit

---
+++ tool_4.js
@@ -0,0 +1,17 @@
+var app= angular.module('toolApp',[]).controller('toolCtrl',function($scope){
+	$scope.mode=2;
+	$scope.rows=[{id: 0,show:true,name:'',value:''}];
+	$scope.is_selected=function(d){return d==$scope.mode?'active':'';}
+	$scope.set_selected=function(d){$scope.mode=d;}
+	$scope.add=function(){
+		obj={
+			id: $scope.rows.length,
+			show: true,
+			name: '',
+			value: ''};
+		$scope.rows.push(obj);
+	}
+	$scope.hide=function(d){
+		$scope.rows[d].show=false;
+	}
+});
